a2fe0bfc0f103f31988d44a6962283624e6d261c
OneBrunou <ms2689992@gmail.com>
2026-06-03 16:15:37 -0300

    Chore:Terminando de configurar o repositorio usuario
    * Feat: Criando repositorio Usuario
    
    * Chore: Terminando de configurar o repositorio usuario

--- ProjetoLoginIB/Repositorio/UsuarioRepositorio.cs
+++ ProjetoLoginIB/Repositorio/UsuarioRepositorio.cs
@@ -28,7 +28,17 @@ namespace ProjetoLoginIB.Repositorio
             using var reader = cmd.ExecuteReader();
             if(reader.Read())
             {
-
+                string senhaBanco = reader["Senha"].ToString()!;
+                if(BCrypt.Net.BCrypt.Verify(senha,senhaBanco))
+                {
+                    return new LoginViewModel
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Nome = reader["Nome"].ToString()!,
+                        Email = reader["Email"].ToString()!,
+                        Nivel = reader["Nivel"].ToString()!
+                    };
+                }
             }
         }
     }
