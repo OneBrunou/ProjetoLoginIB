d91f4f685fa024873666619cb89859063c68283a
igor <igorc7871@gmail.com>
2026-06-03 14:52:05 -0300

    Chore: Configurando as interfaces

---
+++ ProjetoLoginIB/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,9 @@
+﻿using ProjetoLoginIB.Models;
+
+namespace ProjetoLoginIB.interfaces
+{
+    public class IUsuarioRepositorio
+    {
+        LoginViewModel Validar(string Email, string Senha);
+    }
+}
